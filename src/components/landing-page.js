3fb768ce52dede6bfc366510a67cd1def626d623
Jacob Neilands <neilandsjacob@gmail.com>
2021-04-24 14:40:10 +1000

    This is the framework for what needs to happen next

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 class LandingPage extends Component {
   constructor(props) {
@@ -10,27 +10,51 @@ class LandingPage extends Component {
   handleSubmit(event) {
     this.props.handleLogoutSubmit(event);
   }
+
   render() {
     const name = this.props.name;
     const editor = this.props.editor;
     return (
-      <div>
-        <div className = "header">
-        <header className = "header">
-          <h1>UoNBegin</h1>
-          <p>Welcome {editor ? 'Editor ' : 'Student '}{name}</p>
-        </header>
-        <nav>
-          
-        </nav>
+      <Router>
+        <div>
+          <div className = "header">
+          <header className = "header">
+            <h1>UoNBegin</h1>
+            <p>Welcome {editor ? 'Editor ' : 'Student '}{name}</p>
+          </header>
+          <nav>
+            <Link to ="/">University Services</Link>//Uni services and how to access information
+            <Link to ="/directory">Staff Directory</Link>//"Directory" for the faculty, discipline and school
+            <Link to ="/week-one-survival-pack">Week 1 Survival Pack</Link>//Week 1 Survival Pack
+            <Link to ="/university-expectations">What to expect at University</Link>//What uni looks and feels like
+            <Link to ="/faqs-and-testimonials">FAQs and Testionials</Link>//Faqs and Testimonials
+          </nav>
+          </div>
+          <footer id = "footer">
+            <p>C3303694</p>
+            <form onSubmit = {this.handleSubmit}>
+              <input type = "submit" value = "Logout" />
+            </form>
+          </footer>
         </div>
-        <footer id = "footer">
-          <p>C3303694</p>
-          <form onSubmit = {this.handleSubmit}>
-            <input type = "submit" value = "Logout" />
-          </form>
-        </footer>
-      </div>
+        <Switch>
+          <Route path = "/">
+            
+          </Route>
+          <Route path = "/directory">
+
+          </Route>
+          <Route path = "/week-one-survival-pack">
+
+          </Route>
+          <Route path = "/university-expectations">
+
+          </Route>
+          <Route path = "/faqs-and-testimonials">
+
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
